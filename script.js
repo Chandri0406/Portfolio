a717cd1c5295e204ebef321b8b24b880f915f717
Chandri0406 <106561939+Chandri0406@users.noreply.github.com>
2024-09-23 20:46:37 +0200

    images WIP

--- script.js
+++ script.js
@@ -36,3 +36,26 @@ const observer = new IntersectionObserver((entries, observer) => {
 }, observerOptions);
 
 observer.observe(skillSection);
+
+let slideIndex = [1, 1, 1, 1];
+let slideId = ["slideshow_lms", "slideshow_las", "slideshow_wed", "slideshow_bug"];
+
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+    let i;
+    let slides = document.getElementsByClassName(slideId[no]);
+    if (n > slides.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex[no]-1].style.display = "block";  
+}
